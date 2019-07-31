160d2d785402ed053c7c06442a3a979d7bc9d20a
krokerdile <crazykroker@gmail.com>
2019-07-31 23:00:20 +0900

    second commit

--- hwproject/settings.py
+++ hwproject/settings.py
@@ -124,3 +124,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'hw', 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
